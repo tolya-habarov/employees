1bdd3ccaada9248b0de0834d101370506dde90d0
tolya-habarov <41547021+tolya-habarov@users.noreply.github.com>
2022-07-26 22:51:47 +0700

    Fix mongo init datetime.
    * Fix mongo init datetime.
    
    * Some fixes.

--- data/init.sh
+++ data/init.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-mongoimport --username ${MONGO_INITDB_ROOT_USERNAME} --password ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase admin --jsonArray --db ${MONGO_INITDB_DATABASE} --collection employees < /tmp/employees.json
+mongoimport \
+    --username ${MONGO_INITDB_ROOT_USERNAME} \
+    --password ${MONGO_INITDB_ROOT_PASSWORD} \
+    --authenticationDatabase admin \
+    --jsonArray \
+    --db ${MONGO_INITDB_DATABASE} \
+    --collection employees < /tmp/employees.json && \
+mongo \
+    --username ${MONGO_INITDB_ROOT_USERNAME} \
+    --password ${MONGO_INITDB_ROOT_PASSWORD} <<EOF
+    use $MONGO_INITDB_DATABASE;
+    db.employees.updateMany(
+        {},
+        [
+            {
+                \$set: { join_date: { \$dateFromString: { dateString: "\$join_date" } } }
+            }
+        ]
+    );
+EOF
